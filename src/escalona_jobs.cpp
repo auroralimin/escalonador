2f15f7002fbf22667f7e10de253f9c4efe5f474e
Aurora Wang <auroralimin14@gmail.com>
2018-05-29 23:37:39 -0300

    Change scheduler to not busy wait
    Before this commit, the scheduler worked in a busy waiting manner.
    This commit changes it and now the scheduler is blocked when there
    is nothing to do.

--- src/escalona_jobs.cpp
+++ src/escalona_jobs.cpp
@@ -12,6 +12,68 @@ pid_t currentPid = 0;
 std::list<struct job> queues[N_QUEUES];
 std::list<struct job> finishedJobs;
 
+void cleanQueues() {
+    for (int i = 0; i < N_QUEUES; i++) {
+        while ((!queues[i].empty()) && (queues[i].front().finished)) {
+            queues[i].pop_front();
+        }
+    }
+}
+
+int firstQueueNotEmpty() {
+    cleanQueues();
+    for (int i = 0; i < N_QUEUES; i++) {
+        if (!queues[i].empty()) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void runJob() {
+    int index = firstQueueNotEmpty();
+    if (index == -1) return;
+
+    executing = true;
+    struct job& job = queues[index].front();
+    if (job.pid == 0) {
+#ifdef DEBUG
+        std::cout << DEBUG_PRINT(magenta) << "Executed for the 1sr time"
+            << std::endl;
+#endif
+        job.descending = true;
+        job.oldQueue = false;
+        job.finished = false;
+        pid_t pid = fork();
+        if (pid == 0) {
+            if (execl(job.file, job.file, NULL) == -1) {
+                std::cerr << "Nao conseguiu executar " << job.file
+                    << std::endl;
+                std::cerr << ERROR_PRINT << strerror(errno)
+                    << std::endl;
+            }
+        } else {
+            job.pid = pid;
+        }
+    } else {
+#ifdef DEBUG
+        std::cout << DEBUG_PRINT(magenta) << "Continued process: " << job.pid
+            << std::endl;
+#endif
+        kill(job.pid, SIGCONT);
+    }
+
+    currentPriority = job.priority;
+    currentPid = job.pid;
+    alarm(QUANTUM);
+#ifdef DEBUG
+    std::cout << DEBUG_PRINT(magenta) << "Executed proccess: " << job.pid
+        << std::endl;
+    std::cout << DEBUG_PRINT(magenta) << "Priority = " << job.priority + 1
+        << std::endl;
+#endif
+}
+
 void jobQuantum(int) {
 #ifdef DEBUG
     std::cout << DEBUG_PRINT(magenta) << "Quantum! " << currentPid << std::endl;
@@ -34,17 +96,19 @@ void jobQuantum(int) {
     queues[currentPriority].front().finished = true;
 
     executing = false;
+    runJob();
 }
 
 void jobFinished(int) {
     pid_t pid;
+    bool isRunning = true;
     int status;
+	int alarmRemain = alarm(0);
 
     while ((pid = waitpid(-1, &status, WUNTRACED | WNOHANG)) > 0) {
         if (WIFSTOPPED(status) || WIFCONTINUED(status)) {
             continue;
         }
-        alarm(0);
 #ifdef DEBUG
         std::cout << DEBUG_PRINT(magenta) << "Finished " << pid << std::endl;
 #endif
@@ -69,8 +133,16 @@ void jobFinished(int) {
         endTime = endTime.substr(0, endTime.size() - 1);
         strcpy(job.endTime, endTime.c_str());
         finishedJobs.emplace_back(job);
-        executing = false;
+        if (pid == currentPid) {
+            executing = false;
+            isRunning = false;
+        }
     }
+    if (!isRunning) { 
+	   	runJob();
+	} else {
+		alarm(alarmRemain);
+	}	
 }
 
 void eShutdown(int) {
@@ -80,21 +152,21 @@ void eShutdown(int) {
     for (auto job : finishedJobs) {
         buffer.job = job;
         if (msgsnd(mbId, (void*)&buffer, sizeof(buffer.job), 0) ==
-            -1) {
+                -1) {
             std::cerr << "Nao conseguiu enviar o job para shutdown"
-                      << std::endl;
+                << std::endl;
             std::cerr << ERROR_PRINT << strerror(errno) << std::endl;
         }
 #ifdef DEBUG
         std::cout << DEBUG_PRINT(magenta) << "Sending finished job: " << job.pid
-                  << std::endl;
+            << std::endl;
 #endif
     }
 
     buffer.job.pid = -1;
     if (msgsnd(mbId, (void*)&buffer, sizeof(buffer.job), 0) == -1) {
         std::cerr << "Nao conseguiu enviar o job para finalizar shutdown"
-                  << std::endl;
+            << std::endl;
         std::cerr << ERROR_PRINT << strerror(errno) << std::endl;
     }
 
@@ -108,15 +180,15 @@ void eShutdown(int) {
 
             buffer.job = queues[i].front();
             if (msgsnd(mbId, (void*)&buffer, sizeof(buffer.job), 0) ==
-                -1) {
+                    -1) {
                 std::cerr << "Nao conseguiu enviar o job para shutdown"
-                          << std::endl;
+                    << std::endl;
                 std::cerr << ERROR_PRINT << strerror(errno) << std::endl;
             }
 #ifdef DEBUG
             std::cout << DEBUG_PRINT(magenta)
-                      << "Sending unfinished job: " << buffer.job.pid
-                      << std::endl;
+                << "Sending unfinished job: " << buffer.job.pid
+                << std::endl;
 #endif
             if (buffer.job.pid) {
                 kill(buffer.job.pid, SIGTERM);
@@ -128,36 +200,18 @@ void eShutdown(int) {
     buffer.job.pid = -1;
     if (msgsnd(mbId, (void*)&buffer, sizeof(buffer.job), 0) == -1) {
         std::cerr << "Nao conseguiu enviar o job para finalizar shutdown"
-                  << std::endl;
+            << std::endl;
         std::cerr << ERROR_PRINT << strerror(errno) << std::endl;
     }
 
     exit(0);
 }
 
-void cleanQueues() {
-    for (int i = 0; i < N_QUEUES; i++) {
-        while ((!queues[i].empty()) && (queues[i].front().finished)) {
-            queues[i].pop_front();
-        }
-    }
-}
-
-int firstQueueNotEmpty() {
-    cleanQueues();
-    for (int i = 0; i < N_QUEUES; i++) {
-        if (!queues[i].empty()) {
-            return i;
-        }
-    }
-    return -1;
-}
-
 int main(int argc, char** argv) {
     mbId = msgget(MAILBOX, MAIL_PERMISSION);
     if (mbId == -1) {
         std::cerr << "Nao conseguiu abrir a caixa postal, chave: " << MAILBOX
-                  << std::endl;
+            << std::endl;
         std::cerr << ERROR_PRINT << strerror(errno) << std::endl;
         exit(EXIT_FAILURE);
     }
@@ -171,15 +225,23 @@ int main(int argc, char** argv) {
     signal(SIGUSR2, eShutdown);
 
 #ifdef DEBUG
-    std::cout << DEBUG_PRINT(magenta) << "escalona ppid = " << getpid() << std::endl;
+    std::cout << DEBUG_PRINT(magenta) << "escalona ppid = "
+              << getpid() << std::endl;
 #endif
     struct bufferJob buffer;
     while (true) {
-        while (msgrcv(mbId, (void*)&buffer, sizeof(buffer.job), MSG_JOB,
-                      IPC_NOWAIT) > 0) {
+        if (msgrcv(mbId, (void*)&buffer,
+                  sizeof(buffer.job), MSG_JOB, 0) == -1) {
+            if (errno != EINTR) {
+                std::cerr << "Problema de comunicacao entre caixas postais"
+                    << std::endl;
+                std::cerr << ERROR_PRINT << strerror(errno) << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        } else {
 #ifdef DEBUG
-            std::cout << DEBUG_PRINT(magenta) << "Received msg: " << buffer.job.file
-                      << std::endl;
+            std::cout << DEBUG_PRINT(magenta) << "Received msg: "
+                      << buffer.job.file << std::endl;
 #endif
             auto cTime = std::chrono::system_clock::now();
             std::time_t iTime = std::chrono::system_clock::to_time_t(cTime);
@@ -187,54 +249,7 @@ int main(int argc, char** argv) {
             initTime = initTime.substr(0, initTime.size() - 1);
             strcpy(buffer.job.initTime, initTime.c_str());
             queues[buffer.job.priority].push_back(buffer.job);
-        }
-        if (errno != ENOMSG) {
-            std::cerr << "Problema de comunicacao entre caixas postais"
-                      << std::endl;
-            std::cerr << ERROR_PRINT << strerror(errno) << std::endl;
-            exit(EXIT_FAILURE);
-        }
-
-        int index = firstQueueNotEmpty();
-        if ((index != -1) && (!executing)) {
-            executing = true;
-            struct job& job = queues[index].front();
-            if (job.pid == 0) {
-#ifdef DEBUG
-                std::cout << DEBUG_PRINT(magenta) << "Executed for the 1sr time"
-                          << std::endl;
-#endif
-                job.descending = true;
-                job.oldQueue = false;
-                job.finished = false;
-                pid_t pid = fork();
-                if (pid == 0) {
-                    if (execl(job.file, job.file, NULL) == -1) {
-                        std::cerr << "Nao conseguiu executar " << job.file
-                                  << std::endl;
-                        std::cerr << ERROR_PRINT << strerror(errno)
-                                  << std::endl;
-                    }
-                } else {
-                    job.pid = pid;
-                }
-            } else {
-#ifdef DEBUG
-                std::cout << DEBUG_PRINT(magenta) << "Continued process: " << job.pid
-                          << std::endl;
-#endif
-                kill(job.pid, SIGCONT);
-            }
-
-            currentPriority = job.priority;
-            currentPid = job.pid;
-            alarm(QUANTUM);
-#ifdef DEBUG
-            std::cout << DEBUG_PRINT(magenta) << "Executed proccess: " << job.pid
-                      << std::endl;
-            std::cout << DEBUG_PRINT(magenta) << "Priority = " << job.priority + 1
-                      << std::endl;
-#endif
+            if (!executing) runJob();
         }
     }
 }
